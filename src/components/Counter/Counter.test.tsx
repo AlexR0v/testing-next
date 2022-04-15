ef557bd54bbce81e0c6d69a9d5a94738a327c5c1
aleksrov <mail.ponomarev@yandex.ru>
2022-04-15 13:13:31 +0300

    test: test

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -4,6 +4,7 @@ import Counter from './Counter'
 describe('Counter component', () => {
   test('default counter', () => {
     render(<Counter defaultCount={0} defaultDescription='Test' />)
+    const a: string = 5
     expect(screen.getByText('Current count: 0')).toBeInTheDocument()
     expect(screen.getByText('Desc: Test - DC: 0')).toBeInTheDocument()
   })
