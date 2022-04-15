40bf97146fe2b22081171474409d485d5cd6d491
aleksrov <mail.ponomarev@yandex.ru>
2022-04-15 11:14:41 +0300

    add eslint

---
+++ src/components/Hello.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import Hello from "./Hello";
+
+test("render hello", () => {
+  render(<Hello />);
+  const element = screen.getByText(/hello/i);
+  expect(element).toBeInTheDocument();
+});
